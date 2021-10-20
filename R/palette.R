cdaa5f0d08d31eae91a6bd1037d6e5d0f9968338
myan0065 <80817413+myan0065@users.noreply.github.com>
2021-10-21 10:24:26 +1100

    updated description

--- R/palette.R
+++ R/palette.R
@@ -1,11 +1,18 @@
 # Define colour palette variables and functions here
-
 NAME_colours <- list(
   # add your colours as a named list here, i.e.
   "pink" = "#ea5f94"
 )
 
 
+#' Title
+#'
+#' @param direction
+#'
+#' @return
+#' @export
+#'
+#' @examples
 NAME_palette <- function(direction = 1) {
   # the palette function returns a function,
   # so it would be called like NAME_palette()(2) to generate
